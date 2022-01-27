c5a38451eec2a7ed53bbacbd9e24290e4f2ffce0
srinath178 <97936356+srinath178@users.noreply.github.com>
2022-01-27 11:08:21 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -2,8 +2,8 @@
 class Operators{
       public static void main(String[] args)
       { 
-       System.out.println("Enter the values:");
-       int a=120;
+       System.out.println("Running......");
+       int a=7;
        int b=8;
        System.out.println("output:"+a%b);
        }
