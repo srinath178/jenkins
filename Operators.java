fd7ecf24a92ec60a58a8e6e5a18213b3fb4cd89c
srinath178 <97936356+srinath178@users.noreply.github.com>
2022-01-27 10:12:46 +0530

    c1

---
+++ Operators.java
@@ -0,0 +1,10 @@
+
+class Operators{
+      public static void main(String[] args)
+      { 
+       System.out.println("Enter the values:");
+       int a=4;
+       int b=8;
+       System.out.println("output:"+a%b);
+       }
+}
