a2def27f196c4ff45e91f4e41e293fff3677e377
srinath178 <97936356+srinath178@users.noreply.github.com>
2022-01-27 11:06:20 +0530

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -3,7 +3,7 @@ class Operators{
       public static void main(String[] args)
       { 
        System.out.println("Enter the values:");
-       int a=4;
+       int a=120;
        int b=8;
        System.out.println("output:"+a%b);
        }
